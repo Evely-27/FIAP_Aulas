72b8a4a3bab706848f267bd5a3f0318b52cdddc2
Evely-27 <evelysantos27@gmail.com>
2022-04-28 11:06:28 -0300

    estudando finções arrow funtion e anonima

--- catFilmes/js/funcoes.js
+++ catFilmes/js/funcoes.js
@@ -9,17 +9,49 @@ function minhaprimeiraFuncao(){
     // return a + b
     return `${a} + ${b} = ${a + b}` 
 }
-
-
-let s = Math.floor(Math.random() * 1000)
-function segundaFuncao(){
-    let c = Math.floor(Math.random() * 1000)
-    if(s > c ) {
-        return `${s} - ${c} = ${s + c}` 
-    } else {
-        return `${s} + ${c} = ${s + c}`
-    }
-    
-}
 console.log(minhaprimeiraFuncao())
-console.log(segundaFuncao())
+
+
+// let s = Math.floor(Math.random() * 1000)
+// function segundaFuncao(){
+//     let c = Math.floor(Math.random() * 1000)
+//     if(s > c ) {
+//         // shift + ( = coloca entre parenteses
+
+//         // return `${s} - ${c} = ${s + c}` 
+//         // não precisa obrigatoriamente ter um retorno
+//         console.log(`${s} - ${c} = ${s + c}`)
+//     } else {
+//         // return `${s} + ${c} = ${s + c}`
+//         console.log(`${s} + ${c} = ${s + c}`)
+//     }
+    
+// }
+// console.log(minhaprimeiraFuncao())
+// console.log(segundaFuncao())
+// minhaprimeiraFuncao()
+
+//! esta é uma funcao anonima = não tem nome na função, ela recebe uma função 
+const minhaSegundaFunc = function( ) {
+    return 'Escrevendo minha função anonima'
+}
+
+console.log(minhaSegundaFunc())
+
+// !esta é uma arrow function = representa uma função por meio da seta =>,simplifica a função 
+// paramentros= informaçoes que queremos passar pra funçar utilizar
+// exemplos de fazer= const j = _ => '' / const j = (nome,idade)=> / const j = nome => ''
+
+const j = () => {
+    return 'esta é uma arrow function'
+}
+console.log(j())
+
+const w = nome => `Ola ${nome}, esta é uma arrow function`
+
+console.log(w('Evely'))
+
+const w2 = (nome) => {
+    return `Ola ${nome}, esta é a arrow function w2`
+}
+console.log(w2('Evely Santos'))
