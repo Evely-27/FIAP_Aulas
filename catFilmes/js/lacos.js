e464a1386503a9315c3e70d0b5a89d8173aaae9e
Evely-27 <evelysantos27@gmail.com>
2022-04-28 11:23:04 -0300

    for de forma manual

---
+++ catFilmes/js/lacos.js
@@ -0,0 +1,19 @@
+// ! Laços de REpetição:
+
+//!             Exemplo de Tabuadas
+//* For
+// i = indexador, vai controlar o numero de vezes
+
+// Forma manual do for 
+// let a = 0
+// console.log(a = 0 + 1)  //a = 1
+// console.log(a = a + 1) // a = 1 + 1 => a = 2
+// console.log(a += 1) // a = 2 + 1 => a = 3
+// console.log(++a) // a = 3 + 1 => a = 4
+// console.log('Forma manual do for.')
+for (let i = 0; i <= 10; i ++)
+
+// * While
+//* While ... Do
+
+ 
