c1c31b5f687408d39d20beeef9d86eb7e1f92b27
Evely-27 <evelysantos27@gmail.com>
2022-04-28 15:16:43 -0300

    editando lista de imagem e titulos de filmes

--- catFilmes/js/index.js
+++ catFilmes/js/index.js
@@ -1,1 +1,12 @@
+let  listaDeFilmes = [
+    'https://images-na.ssl-images-amazon.com/images/I/51oXu7N-AOL.jpg',
+    'https://media.fstatic.com/dxYlAYTTnAOGgZBBHHHHOoDp2zs=/290x478/smart/media/movies/covers/2021/06/kc0YEjL5iLjEbedhpqqpaaUEPZa.jpg',
+    'https://cdn.fstatic.com/media/movies/covers/2018/10/2111834923.jpg'
 
+
+]
+let listaDeTitulos = [
+    'Crespúsculo',
+    'RED: Crescer é uma fera',
+    'Missão Impossível: Efeito Fallout'
+]
