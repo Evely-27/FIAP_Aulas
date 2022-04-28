0b2dcbbbccc0e0e6a055dd936db2bd4a2349cc2b
Evely-27 <evelysantos27@gmail.com>
2022-04-28 14:40:05 -0300

    exemplos de condicionais

---
+++ catFilmes/js/condicionais.js
@@ -0,0 +1,23 @@
+let b 
+
+
+if (b > 70) {
+    console.log('O valor de b esta na metade maior da sequência ')
+}else if ( b === 70) {
+    console.log('O valor de b é iguail a 70')
+} else {
+    console.log('O valor de b esta  na metade menor da sequincia')
+}
+
+//! Diferenla entre == e ===
+
+let a = 50
+let d = 50 
+let f = '50'
+
+// console.log(`Esta comparação é : ${a == d}, ou seja ${a} = ${a} = ${d}!`) // ? estmmos fazendo comparação com dois iguais, comparando valores
+// console.log(`Esta comparação é : ${a == f}, ou seja ${a} = ${a} = ${f}!`) // ? estmmos fazendo comparação com dois iguais, comparando valores
+
+
+console.log(`Esta comparação é : ${a === d}, ou seja ${a} = ${a} = ${d}!`) // ? estmmos fazendo comparação com dois iguais, comparando valores e tipos
+console.log(`Esta comparação é : ${a === f}, ou seja ${a} = ${a} !==(diferente) ${f}!`) // ? estmmos fazendo comparação com dois iguais, comparando valores e tipos
