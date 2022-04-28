22245e22c0a114ae0bc0283b7b012b635b42ca89
Evely-27 <evelysantos27@gmail.com>
2022-04-28 11:42:25 -0300

    construindo laço de repetição for tabuada

--- catFilmes/js/lacos.js
+++ catFilmes/js/lacos.js
@@ -11,8 +11,21 @@
 // console.log(a += 1) // a = 2 + 1 => a = 3
 // console.log(++a) // a = 3 + 1 => a = 4
 // console.log('Forma manual do for.')
-for (let i = 0; i <= 10; i ++)
 
+//  for com j incrementando com i
+for (let i = 0; i <= 10; i ++) {
+   for(let j = 0; j <= i ; j ++){
+    console.log(j)
+   }
+    console.log(i)
+}
+
+for (let i = 0; i <= 10; i ++) {
+    for(let j = 0; j <= 10 ; j ++){
+     console.log(`${i} * ${j} = ${i * j}`)
+    }
+     
+ }
 // * While
 //* While ... Do
 
