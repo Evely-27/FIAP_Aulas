1dbfbeef2ff84978162b2fe239b0d4ecdc0d043a
Evely-27 <evelysantos27@gmail.com>
2022-05-11 12:20:47 -0300

    concertando erro de thead e e body na table

--- ProjetosReact/projetoreactapp/src/Tabela.js
+++ ProjetosReact/projetoreactapp/src/Tabela.js
@@ -1,6 +1,13 @@
+import axios from "axios";
+import {useEffect, useState} from "react"; // useEffect = tipo um construtor,fica chamnda a função de forma automatica, é usado quando a pagina for executada, useState= deixa manipular o conteudo da variavel em tempo de execução
+
+//carreguei a pagina, quero q carrega o axios
+
+
 
 function Tabela() {
-// vamos contruir uma conts para deicar dinamico
+
+    // vamos contruir uma conts para deixar dinamico, mais localmente
 const lista_usuarios = [ //criamos uma const deita de array, que criamos objts joson dentro
     {id: 1, nome:'evely', email: 'evely_santos.com'},
     {id: 2, nome:'Nando', email: 'nando_santis.com'},
@@ -8,24 +15,46 @@ const lista_usuarios = [ //criamos uma const deita de array, que criamos objts j
     {id: 4, nome:'Luis',  email: 'luis_carlos.com'},
 ]
     return ( //esta retornando um componente
-        <table className='minhaTabela'>
-              <tr>  {/* linha */}
-                  <th>Id</th>  {/* conteudo do cabeçalho */}
-                  <th>Nome</th>
-                  <th>Email</th>
-              </tr>
-              {/* a chave {} é para escrever o js dentro do componente */}
-            {lista_usuarios.map( usuario => { //função map= lop,percorre um arry, dai criamos uma arrow function e jogamos o que queremos que seja jogado na variavel usuario
-                //console.log(usuario);
-                return <tr>
-                        <td>{usuario.id}</td> {/* td = conteudo da linha */}
-                        <td>{usuario.nome}</td>
-                        <td>{usuario.email}</td>
-                 </tr>
-            })}
-             
+
+    <table className='minhaTabela'>
+    <thead>
+        <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Email</th>
+        </tr>
     
-          </table>
+    </thead>  
+    <tbody> 
+        { lista_usuarios.map( usuario => {
+            return (
+                <tr key={usuario.id}>
+                    <td>{usuario.id}</td>
+                    <td>{usuario.nome}</td>
+                    <td>{usuario.email}</td>
+                </tr>
+                )
+        } ) }
+    </tbody>
+  </table>
+
+        // <table className='minhaTabela'>
+        //       <tr>  {/* linha */}
+        //           <th>Id</th>  {/* conteudo do cabeçalho */}
+        //           <th>Nome</th>
+        //           <th>Email</th>
+        //       </tr>
+        //       {/* a chave {} é para escrever o js dentro do componente */}
+        //     {lista_usuarios.map( usuario => { //função map= lop,percorre um arry, dai criamos uma arrow function e jogamos o que queremos que seja jogado na variavel usuario
+        //         //console.log(usuario);
+        //         return <tr key={usuario.id}> {/* o key server para colocarmos uma indentificação em cada lista */}
+        //                 <td>{usuario.id}</td> {/* td = conteudo da linha */}
+        //                 <td>{usuario.nome}</td>
+        //                 <td>{usuario.email}</td>
+        //          </tr>
+        //     })}
+             
+        //   </table>
     );
 }
 
