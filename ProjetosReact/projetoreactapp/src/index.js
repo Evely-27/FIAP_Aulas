fd5b314cd491ca77b56865b690aca01b47171aba
Evely-27 <evelysantos27@gmail.com>
2022-05-11 10:48:59 -0300

    comentado uma explicacao do root

--- ProjetosReact/projetoreactapp/src/index.js
+++ ProjetosReact/projetoreactapp/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // esta sincronizando a componente root(q esta no index.html) e jogando esse componente app
 root.render(
   <React.StrictMode>
     <App />
